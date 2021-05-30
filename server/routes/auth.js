24f744ac3180edc47fd07496e1bebf564d07a87a
Quang Tran <quangtd@dgroup.co>
2021-05-30 09:26:58 +0700

    user

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,37 @@
 const express = require("express");
-
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+
+const { authCheck } = require("../middlewares/auth");
+
+const { createUser, getUserByToken } = require("../controllers/auth");
+
+// @route    POST api/users
+// @desc     Register user
+// @access   Public
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.post(
+  "/users",
+  [
+    check("name", "Name is required").notEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  createUser
+);
+
+// @route    GET api/auth
+// @desc     Get user by token
+// @access   Private
+router.get("/auth", authCheck, getUserByToken);
 
 //import
 const { createOrUpdateUser } = require("../controllers/auth");
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,29 +1,9 @@
-const express = require("express");
-const router = express.Router();
-const { check, validationResult } = require("express-validator");
-
-const { createUser } = require("../controllers/users");
-
-// @route    POST api/users
-// @desc     Register user
-// @access   Public
+var express = require("express");
+var router = express.Router();
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
-
-router.post(
-  "/users",
-  [
-    check("name", "Name is required").notEmpty(),
-    check("email", "Please include a valid email").isEmail(),
-    check(
-      "password",
-      "Please enter a password with 6 or more characters"
-    ).isLength({ min: 6 }),
-  ],
-  createUser
-);
 
 module.exports = router;
