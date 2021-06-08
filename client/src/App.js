f923a2f5e80634e09944c00963b8441a93be1ce9
Quang Tran <quangtd@dgroup.co>
2021-06-08 16:19:36 +0700

    FE: create react app

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div class="app">
+      <h1>do an tot nghiep</h1>
+    </div>
+  );
+};
+
+export default App;
